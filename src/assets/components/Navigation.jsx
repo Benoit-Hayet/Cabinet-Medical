ea7b46a8eda80b2c7ea2c4450e96826d22468001
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-14 22:52:32 +0100

    Clean Navbar

--- src/assets/components/Navigation.jsx
+++ src/assets/components/Navigation.jsx
@@ -10,38 +10,45 @@ import {
   MDBNavbarLink,
   MDBCollapse
 } from 'mdb-react-ui-kit';
+import LogoSmall from "../logo/LogoSmall.png";
 
 export default function Navigation() {
   const [openNav, setOpenNav] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
+    <MDBNavbar expand='lg' light bgColor='light' style={{ height: '80px', paddingTop: '20px' }}>
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#'></MDBNavbarBrand>
+        <MDBNavbarBrand href='#' style={{ color: '#74C274', marginRight: 'auto' }}>
+          <img src={LogoSmall} className="d-block" alt="Small Logo" style={{ width: '200px', height: 'auto' }} />
+        </MDBNavbarBrand>
         <MDBNavbarToggler
           type='button'
           aria-expanded='false'
           aria-label='Toggle navigation'
           onClick={() => setOpenNav(!openNav)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon='bars' fas style={{ color: '#74C274' }} />
         </MDBNavbarToggler>
-        <MDBCollapse navbar open={openNav}>
-          <MDBNavbarNav>
+        <MDBCollapse navbar open={openNav} className="justify-content-center">
+          <MDBNavbarNav style={{ justifyContent: "center", alignItems: "center" }}>
             <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='/Home'>
+              <MDBNavbarLink active aria-current='page' href='/Home' style={{ color: '#74C274' }}>
                 Accueil
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href='/temoignages'>Témoignages</MDBNavbarLink>
+              <MDBNavbarLink href='/temoignages' style={{ color: '#74C274' }}>
+                Témoignages
+              </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Qui sommes-nous ?</MDBNavbarLink>
+              <MDBNavbarLink href='#' style={{ color: '#74C274' }}>
+                Qui sommes-nous ?
+              </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-              Galerie
+              <MDBNavbarLink disabled style={{ color: '#74C274' }}>
+                Galerie
               </MDBNavbarLink>
             </MDBNavbarItem>
           </MDBNavbarNav>
