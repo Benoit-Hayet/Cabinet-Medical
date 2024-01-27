958b6488c3dc51439dbb9cd840e966d0ba50e49b
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-27 19:19:14 +0100

    First clean footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -36,21 +36,19 @@ export default function Footer() {
           <MDBRow className='mt-3'>
             <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                Company name
+                <MDBIcon className="me-3" />
+                Cabinet Médical
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit.
+                Une équipe attentionnée et a l'écoute située au coueur de Bordeaux.
               </p>
             </MDBCol>
 
             <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <h6 className='text-uppercase fw-bold mb-4'>INFOS PRATIQUES</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
+              <MDBIcon icon="map-marker-alt" className="me-2" />
+              139 Avenue de Bordeaux <br/>33000 Bordeaux
               </p>
               <p>
                 <a href='#!' className='text-reset'>
@@ -96,12 +94,12 @@ export default function Footer() {
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
               <p>
-                <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
+              <MDBIcon icon="map-marker-alt" className="me-2" />
+              139 Avenue de Bordeaux <br/>33000 Bordeaux
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
+                cabinetinfirmière@example.com
               </p>
               <p>
                 <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
