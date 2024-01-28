d65b8ca3cf718ea678d8e2fb8ed8b3ed8b01e754
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-28 22:48:15 +0100

    Cabinet page

--- src/assets/pages/Cabinet.jsx
+++ src/assets/pages/Cabinet.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { MDBRipple } from 'mdb-react-ui-kit';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import CabinetPic from "../Pictures/Cabinet/CabinetPic.jpg";
 
-export default function Cabinet() {
+
+export default function Cabinet () {
   return (
     <div>
       <Navigation/>
     <MDBRipple rippleTag='a'>
-      <img
-        src='https://mdbootstrap.com/img/new/standard/city/044.webp'
-        className='img-fluid rounded'
-        alt='example'
-      />
+      <img src={CabinetPic} className='img-fluid rounded'alt='example'/>
     </MDBRipple>
     <Footer/>
     </div>
